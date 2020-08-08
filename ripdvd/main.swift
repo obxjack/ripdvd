3db9f94b4e14e7f800e09f44fa24688679e8adcb
Jackie Barker <jack.barker@gmail.com>
2020-08-08 18:25:26 -0400

    Initial Commit

---
+++ ripdvd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ripdvd
+//
+//  Created by Jackie Barker on 8/8/20.
+//  Copyright © 2020 Jackie Barker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
